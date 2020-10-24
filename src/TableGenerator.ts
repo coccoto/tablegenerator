ef1c768f7866f3b5ee0713a33bffea588cbbc4dc
coccoto <soprano.lune@gmail.com>
2020-10-24 22:54:38 +0900

    2020-10-24 22:54

--- src/TableGenerator.ts
+++ src/TableGenerator.ts
@@ -11,21 +11,35 @@ export default class TableGenerator {
     }
 
     /**
-     * 日付を差し込む
+     * 日付と曜日を差し込む
+     * 
+     * @return {boolena} success
      */
-    private insert(row: number, tableDate: {[name: string]: string}): void {
+    private insert(row: number, tableDate: string[][]): boolean {
+
+        this.sheet.getRange(row, 1, 1, 2).setValues(tableDate)
+        return true
+    }
+
+    /**
+     * 選択済みレコードをペースト
+     */
+    private push(row: number, selected: Spreadsheet.Range): void {
+
+        const address = this.sheet.getRange(row, 1)
+        selected.copyTo(address)
+    }
+
+    /**
+     * 日付から曜日を取得
+     */
+    private prepareDate(tableDate: {[name: string]: string}): string[][] {
 
         const date = new Date(Number(tableDate.year), Number(tableDate.month) - 1, Number(tableDate.date))
         const day = date.getDay()
         const weeks: string[] = ['日', '月', '火', '水', '木', '金', '土']
 
-        this.sheet.getRange(row, 1, 1, 2).setValues([[tableDate.date, weeks[day]]])
-    }
-
-    private push(row: number, selected: Spreadsheet.Range): void {
-
-        const address = this.sheet.getRange(row, 1)
-        selected.copyTo(address)
+        return [[tableDate.date, weeks[day]]]
     }
 
     /**
@@ -34,22 +48,42 @@ export default class TableGenerator {
     private assemble(tableDate: {[name: string]: string}, tableHeight: number, tableWidth: number, selected: Spreadsheet.Range): void {
 
         const ROW_INIT: number = 3
+        // ループカウンター
         let i: number = ROW_INIT
+        // 現在列
+        let c: number = ROW_INIT
+
+        // 指定された曜日のレコードだけを作成（未指定なら全件作成）
+        const createRule: string = this.sheet.getRange(2, 2).getValue()
+        // 現在日時
+        let currentDate: string[][]
 
         while (i !== tableHeight + ROW_INIT) {
-            tableDate['date'] = String(i - ROW_INIT + 1)
-            this.push(i, selected)
-            this.insert(i, tableDate)
-            i ++
+            // 対応する日付を取得
+            tableDate.date = String(i - ROW_INIT + 1)
+            // 曜日を取得
+            currentDate = this.prepareDate(tableDate)
 
+            // ルールに合致もしくはルール未指定時、レコードを追加
+            if (createRule === currentDate[0][1] || createRule === '') {
+                this.push(c, selected)
+                this.insert(c, currentDate)
+                // 現在列をインクリメント
+                c ++
+            }
             // 最終行
             if (! (i !== tableHeight + ROW_INIT)) {
-                this.sheet.getRange(i, 1).setValue('-')
-                this.sheet.getRange(i, 1, 1, tableWidth).setBorder(true, true, true, true, true, true)
+                this.sheet.getRange(c, 1).setValue('-')
+                this.sheet.getRange(c, 1, 1, tableWidth).setBorder(true, true, true, true, true, true)
             }
+            // ループカウンターをインクリメント
+            i ++
         }
     }
 
+    /**
+     * 貼り付け元のレコードを選択
+     */
     private select(tableWidth: number): Spreadsheet.Range {
 
         const rowFrom: number = 3
@@ -61,7 +95,7 @@ export default class TableGenerator {
     }
 
     /**
-     * テーブルの横幅を取得する
+     * テーブルの横幅を取得
      */
     private tableWidth(i: number = 3): number {
 
@@ -75,7 +109,7 @@ export default class TableGenerator {
     }
 
     /**
-     * 日付から生成するテーブルの縦幅を取得する
+     * 日付から生成するテーブルの縦幅を取得
      */
     private tableHeight(tableDate: {[name: string]: string}): number {
 
@@ -84,7 +118,7 @@ export default class TableGenerator {
     }
 
     /**
-     * テーブルにセットされている年月を取得する
+     * テーブルにセットされている年月を取得
      */
     private tableDate(): {[name: string]: string} {
 
@@ -97,9 +131,12 @@ export default class TableGenerator {
     public main(): void {
 
         const tableDate: {[name: string]: string} = this.tableDate()
+        // 縦幅を取得
         const tableHeight: number = this.tableHeight(tableDate)
+        // 横幅を取得
         const tableWidth: number = this.tableWidth()
 
+        // 列をコピー
         const selected: Spreadsheet.Range = this.select(tableWidth)
 
         this.assemble(tableDate, tableHeight, tableWidth, selected)
